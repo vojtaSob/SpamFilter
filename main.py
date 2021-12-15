2c4348a45a53f44bed5be98001052fb54a42371b
vojtasob <vojta.sob@gmail.com>
2021-12-15 12:52:38 +0100

    repo cleanup

--- main.py
+++
@@ -1,7 +0,0 @@
-from utils import write_to_file, load_from_file, read_classification_from_file, write_classification_to_file
-
-if __name__ == '__main__':
-    s1 = "a"
-    s2 = "b"
-    s3 = s1 + s2
-    print(s3)
